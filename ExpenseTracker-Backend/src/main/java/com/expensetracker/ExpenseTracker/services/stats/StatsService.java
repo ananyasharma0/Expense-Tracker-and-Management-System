59aabef37127bea9c964054c5cebe070c3e25bf6
ananyasharma0 <sharma.ananya1003@gmail.com>
2025-04-22 17:17:03 +0530

    Cleaned up unwanted files

--- ExpenseTracker-Backend/src/main/java/com/expensetracker/ExpenseTracker/services/stats/StatsService.java
+++
@@ -1,9 +0,0 @@
-package com.expensetracker.ExpenseTracker.services.stats;
-
-import com.expensetracker.ExpenseTracker.dto.GraphDTO;
-import com.expensetracker.ExpenseTracker.dto.StatsDTO;
-
-public interface StatsService {
-    GraphDTO getChartData();
-    StatsDTO getStats();
-}
